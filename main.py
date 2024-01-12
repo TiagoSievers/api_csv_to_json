954c461005ce25660450054215f3a7b983adf9ae
Tiago Sievers <65292458+TiagoSievers@users.noreply.github.com>
2024-01-11 23:32:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import pandas
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return"Hello world"
-    
-@app.post("/upload")
+@app.post("/")
 async def upload(file:UploadFile = File(...)):
     df = pandas.read_csv(file.file).T.to_dict()
     return df
