24506fa69fa9d656507da3f2744bf33e8a3aba1b
Tiago Sievers <65292458+TiagoSievers@users.noreply.github.com>
2024-01-12 11:10:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ app = FastAPI()
 
 @app.post("/")
 async def upload(file:UploadFile = File(...)):
-    df = pandas.read_csv(file.file)
+    df = pandas.read_csv(file.file).T.to_dict()
+    json_data = json.dumps(df)
+    return json_data
 
     # Convert DataFrame to JSON
-    json_data = df.to_json(orient='records')
+    #json_data = df.to_json(orient='records')
 
     # Return the JSON data as the response
-    return {"data": json_data}
+    #return {"data": json_data}
